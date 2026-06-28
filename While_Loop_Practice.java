1eedeccf48c80dfec94d91c93be85f805041152c
Aastha Dhiman <aasthadhimann@gmail.com>
2026-06-28 15:15:51 +0530

    Add Java while loop program

---
+++ While_Loop_Practice.java
@@ -0,0 +1,18 @@
+public class While_Loop_Practice {
+    public static void main(String[] args) {
+        
+        // Start at 1 if you want to print numbers from 1 to 10
+        int i = 1; 
+        
+        // Loop will continue running as long as i is less than or equal to 10
+        while (i <= 10) {
+            
+            // Print the actual value of the variable i 
+            System.out.println(i);
+            
+            // Increment i by 1 so the loop progresses. 
+
+            i = i + 1; // : i++;
+        }
+    }
+}
