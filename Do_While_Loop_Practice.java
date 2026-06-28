c025063ab933d21d6af7641215b935f3fbd1f0f0
Aastha Dhiman <aasthadhimann@gmail.com>
2026-06-28 15:19:17 +0530

    Create do-while loop practice file

---
+++ Do_While_Loop_Practice.java
@@ -0,0 +1,17 @@
+public class Do_While_Loop_Practice {
+    public static void main(String[]args) {
+
+        // Initialize the counter variable 'i' at 1
+        int i = 1;
+        do {
+
+            // Print the current numerical value of 'i' to the console
+            System.out.println(i);
+
+            // Increment the value of 'i' by 1 after each print statement
+            i=i+1;
+
+            // The loop will continue to run as long as 'i' is less than or equal to 10
+        } while(i<11);
+    }
+}
