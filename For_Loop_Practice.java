eae01f45b82e2143c5dcd8d992d72eb9a76f72c2
Aastha Dhiman <aasthadhimann@gmail.com>
2026-06-28 15:34:39 +0530

    Create for loop basic counting program

---
+++ For_Loop_Practice.java
@@ -0,0 +1,13 @@
+// Class to practice the concept of For Loops in Java
+public class Java_Loops {
+    
+    public static void main(String[] args) {
+        
+        // Loop starts from i = 1 and runs until i becomes 10 (increments by 1 each time)
+        for (int i = 1; i <= 10; i++) {
+            
+            // Prints the current value of i in each iteration
+            System.out.println( + i);
+        }
+    }
+}
